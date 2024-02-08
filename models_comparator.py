cb23928e223b5b4ea75e92f1e3d8164014c07690
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-08 11:31:27 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -1,134 +1,236 @@
+
+#-------------------------Import libraries-------------------------
+import time
 import streamlit as st
-import os
+from streamlit_pills import pills
+from streamlit_star_rating import st_star_rating
 import openai
-
-st.set_page_config(layout="wide")
-
-with st.sidebar:
-    
-    st.header(" GPT Parameters Set-up")
-    st.markdown("Please, fulfill the following fields to compare GPTs' results and costs and choose the right models.")
-    #api_key = st.text_input(label = ":key: **OpenAI API Key**", value="", placeholder= "Insert here your API Key", type = "password")
-    prompt = st.text_area(label = ":question: **Your Prompt**", value="", max_chars=250, placeholder= "Insert here your prompt")
-    temperature = st.slider(":thermometer: **Temperature**", min_value=0.0, max_value=2.0, value=0.8, help = "Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.")
-    top_p = st.slider(":bar_chart: **Top-P**", min_value=0.01, max_value=1.0, value=1.0, help = "An **alternative** to sampling with temperature where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. Do not use combined with high temperature.")
-    max_tokens = st.slider(":scroll: **Max Tokens**", min_value= 32, max_value= 4096, value =1024, help = "The maximum number of tokens that can be generated.")
+import psycopg2
+#--------------------------OpenAI API Key--------------------------
 
 openai.api_key = st.secrets.openai_key
 
-st.title("💬 Which GPT Model should I use? (Beta)")
-"#"
-with st.expander("How it works :question:"):
-    st.write("Choose the right model can be a messy. Use this application to **compare the outcomes** of different OpenAI Models from ChatCompletion API.")
-    #st.write(":one: Insert your **OpenAI API Key**")
-    st.write(":one: **Type prompt** you want to test")
-    st.write(":two: Set-up prompt **parameters**")
-    st.write(":three: **Compare results** in term of quality and costs")
-    st.write(":four: **Iterate** the process")
+
+#------------------------Page configuration------------------------
+
+st.set_page_config(page_title="GPT Comparator", page_icon="🤖", layout="wide")
+
+#------------------------Model Selection----------------------
+
+model_1 = "gpt-3.5-turbo"
+model_2 = "gpt-4"
+model_3 = "gpt-4-0125-preview"
+
+#------------------------Function Definition-----------------------
+
+def ask_function(model, context, prompt, temperature, top_p, max_tokens):
+  
+  completion = openai.ChatCompletion.create(
+      model = model,
+      temperature = temperature,
+      top_p = top_p ,
+      max_tokens = max_tokens,
+      messages=[
+      {"role": "system", "content": context},
+      {"role": "user", "content": prompt}
+    ]
+  )
+
+  return completion
+
+def set_up_values():
+    st.session_state.context = "You are an helpful assistant"
+    st.session_state.prompt = ""
+    st.session_state.temperature = 0.8
+    st.session_state.top_p = 1
+    st.session_state.max_tokens = 1024
+
+def write_postgres(user_query, system_role, temperature, top_p, max_tokens, model_1, model_2, model_3, answer_1, answer_2, answer_3, token_1, token_2, token_3, love_1, love_2, love_3):
+
+  try:
+    conn = psycopg2.connect(
+      host=st.secrets.host,
+      database=st.secrets.database,
+      user=st.secrets.user,
+      password=st.secrets.pass
+  )
+    cur = conn.cursor()
+
+    # Esecuzione della query SQL per l'inserimento
+    query = f"""INSERT INTO streamlit_gpt_comparator (
+      user_query,
+      system_role,
+      model_1, 
+      model_2, 
+      model_3,
+      temperature,
+      top_p,
+      max_tokens,
+      answer_1,
+      answer_2,
+      answer_3,
+      token_1,
+      token_2,
+      token_3,
+      love_1,
+      love_2,
+      love_3,
+      create_time
     
+   
+      ) 
+      VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP)"""
+    
+    data = (user_query, system_role, temperature, top_p, max_tokens, model_1, model_2, model_3, answer_1, answer_2, answer_3, token_1, token_2, token_3, love_1, love_2, love_3)
+    
+    cur.execute(query, data)
+    conn.commit()
+    
+    st.toast('Thank you! Your feedback has been saved', icon="✅")
 
-#OpenAI API Calling
-
-completion_gpt35 = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    temperature = temperature,
-    top_p = top_p ,
-    max_tokens = max_tokens,
-    messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-  ]
-)
-answer_gpt35 = completion_gpt35.choices[0].message
-
-completion_gpt35_1106 = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo-1106",
-    temperature = temperature,
-    top_p = top_p ,
-    max_tokens = max_tokens,
-    messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-  ]
-)
-answer_gpt35_1106 = completion_gpt35_1106.choices[0].message
-
-completion_gpt40 = openai.ChatCompletion.create(
-    model="gpt-4",
-    temperature = temperature,
-    top_p = top_p ,
-    max_tokens = max_tokens,
-    messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-  ]
-)
-answer_gpt40 = completion_gpt40.choices[0].message
-
-## give_a_score35 = openai.Completion.create(
-##    model="gpt-3.5-turbo-instruct",
-##    prompt = f"""Give me a number between 1 and 5 rating the following answer in terms of coherence with the query, completeness of the answer, grammary Spelling. I want just 1 number as output. Example of answer: 4
-##                 This is the query: {prompt}. This is the answer {answer_gpt35}.""")
-
-## give_a_score351106 = openai.Completion.create(
-##    model="gpt-3.5-turbo-instruct",
-##    prompt = f"""Give me a number between 1 and 5 rating the following answer in terms of coherence with the query, completeness of the answer, grammary Spelling. I want just 1 number as output. Example of answer: 4
-##                 This is the query: {prompt}. This is the answer {answer_gpt35_1106}.""")
-
-##give_a_score_40 = openai.Completion.create(
-##    model="gpt-3.5-turbo-instruct",
-##    prompt = f"""Give me a number between 1 and 5 rating the following answer in terms of coherence with the query, completeness of the answer, grammary Spelling. I want just 1 number as output. Example of answer: 4
-##                 This is the query: {prompt}. This is the answer {answer_gpt40}.""")
-
-
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-  st.header("**gpt-3.5-turbo**")
+  except Exception as e:
+          print(f"Si è verificato un errore: {e}")
   
-  st.markdown("Snapshot of **GPT-3.5-turbo** from **June 13th 2023**. Best in terms of performance / cost ratio for simple textual tasks (**4,096 tokens** as context window).")
-  st.link_button("Model", url="https://platform.openai.com/docs/models/gpt-3-5", help=None, type="secondary", disabled=False, use_container_width=False)
-  cola, colb, colc = st.columns(3)
-  with cola:
-    st.metric(":boom: Tokens", value = str(completion_gpt35["usage"]["completion_tokens"])+"T", help = "Tokens can be thought of as pieces of words. This number refers to *completion tokens*. You should be aware also of *prompt tokens* when evaluating full costs.")
-  with colb:
-    st.metric(":money_with_wings: Cost", value = str(round(completion_gpt35["usage"]["completion_tokens"]*0.002,2))+"$", help ="Cost in $ per 1 thousand operations.")
-  with colc:
-    "#"
- 
-  st.info(answer_gpt35['content'], icon=None)
-  ##score_gpt35="⭐"*int(give_a_score35.choices[0].text)
-  ##st.markdown(f'<div style="text-align: right;">{score_gpt35}</div>', unsafe_allow_html=True)
+  finally:
+    cur.close()
+    conn.close()
+
+  st.session_state.gpt35love = False
+  st.session_state.gpt40love = False
+  st.session_state.gpt41love = False
+  st.session_state.prompt = ""
+
   
-with col2:
-  st.header("**gpt-3.5-turbo-1106**")
-  st.markdown("**GPT-3.5 Turbo** model with **improved instruction following**, JSON mode, reproducible outputs, parallel function calling (**16,385 tokens** as context window).")
-  st.link_button("Model", url="https://platform.openai.com/docs/models/gpt-3-5", help=None, type="secondary", disabled=False, use_container_width=False)
-  cold, cole, colf = st.columns(3)
-  with cold:
-    st.metric(":boom: Tokens", value = str(completion_gpt35_1106["usage"]["completion_tokens"])+"T", help = "Tokens can be thought of as pieces of words. This number refers to *completion tokens*. You should be aware also of *prompt tokens* when evaluating full costs.")
-  with cole:
-    st.metric(":money_with_wings: Cost", value =str(round(completion_gpt35_1106["usage"]["completion_tokens"]*0.002,2))+"$", help ="Cost in $ per 1 thousand operations.")
-  with colf:
-    "#"  
-  st.info(answer_gpt35_1106['content'], icon=None)
-  ##score_gpt_351106="⭐"*int(give_a_score351106.choices[0].text)
-  ##st.markdown(f'<div style="text-align: right;">{score_gpt_351106}</div>', unsafe_allow_html=True)
+  
+#--------------------------Sidebar Section-------------------------
 
-with col3:
-  st.header("**gpt-4**")
-  st.markdown("GPT-4 is a **large multimodal model** optimized for chat but works well for traditional completions tasks with advanced reasoning capabilities (**8,192 tokens** as context window).")
-  st.link_button("Model", url="https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo", help=None, type="secondary", disabled=False, use_container_width=False)
-  colg, colh, coli = st.columns(3)
-  with colg:
-    st.metric(":boom: Tokens", value = str(completion_gpt40["usage"]["completion_tokens"])+"T", help = "Tokens can be thought of as pieces of words. This number refers to *completion tokens*. You should be aware also of *prompt tokens* when evaluating full costs.")
-  with colh:
-    st.metric(":money_with_wings: Cost", value =str(round(completion_gpt40["usage"]["completion_tokens"]*0.006,2))+"$", help ="Cost in $ per 1 thousand operations.")
-  with coli:
-    "#"  
-  st.info(answer_gpt40['content'], icon=None)
-  ##score_gpt40="⭐"*int(give_a_score_40.choices[0].text)
-  ##st.markdown(f'<div style="text-align: right;">{score_gpt40}</div>', unsafe_allow_html=True)
+def main():
+  with st.sidebar:  
+      
+      with st.form(key='gpt_parameters', border = False):
+        
+        st.header(" GPT Parameters Set-up")
+        st.markdown("Please, fulfill the following fields to compare GPTs' results and choose the right model.")
+        #api_key = st.text_input(label = ":key: **OpenAI API Key**", value="", placeholder= "Insert here your API Key", type = "password")
+        
+        context = st.text_area(label = "🤖 **Role of the system**", key = "context", value="You are an helpful assistant", max_chars=100, placeholder= "You are an helpful assistant", help = "Set the role for the assistant. If you want, specify also the personality (ex: irriverent, funny).")
+        prompt = st.text_area(label = ":question: **Your Prompt**", key = "prompt", value="", max_chars=250, placeholder= "Insert here your prompt", help = "Type here an instruction in natural language." )
+        temperature = st.slider(":thermometer: **Temperature**", key = "temperature", min_value=0.0, max_value=2.0, value=0.8, help = "Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.")
+        top_p = st.slider(":bar_chart: **Top-P**", key = "top_p", min_value=0.01, max_value=1.0, value=1.0, help = "An **alternative** to sampling with temperature where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered. Do not use combined with high temperature.")
+        max_tokens = st.slider(":scroll: **Max Tokens**", key = "max_tokens", min_value= 32, max_value= 1024, value = 1024, help = "The maximum number of tokens that can be generated. In this app they will be limited to 1024 for cost savings and performances")
+        
+        co10, col11, col12, col9,col8 = st.columns([1,1,1,1,1])
+
+        with col9: 
+          submit_button = st.form_submit_button(label=':heavy_check_mark:')
+        with col8:
+          clear_button = st.form_submit_button(label=':x:', on_click = set_up_values)
+          
+  #--------------------------Title--------------------------
+      
+  col1, mid, col2 = st.columns([1,1,30])
+  with col1:
+      st.image('https://static-00.iconduck.com/assets.00/openai-icon-2021x2048-4rpe5x7n.png', width=80)
+  with col2:
+      st.title('Which GPT Model should I use?')
+
+  #-------------------------Main Section-------------------------
+      
+  tabMain, tabInfo = st.tabs(["Main", "Info"])
+
+  with tabMain:
+    
+    with st.spinner("Generating with gpt-3.5-turbo ..."):
+      gpt35 = ask_function("gpt-3.5-turbo", context, prompt, temperature, top_p, max_tokens)
+      answer_gpt35 = gpt35.choices[0].message
+      tokens_gpt35 = gpt35["usage"]["completion_tokens"]
+     
+
+    with st.spinner("Generating with gpt-4 ..."):
+      gpt40 = ask_function("gpt-4", context, prompt, temperature, top_p, max_tokens)
+      answer_gpt40 = gpt40.choices[0].message
+      tokens_gpt40 = gpt40["usage"]["completion_tokens"]
+
+    with st.spinner("Generating with gpt-4-0125-preview..."):
+      gpt40_125 = ask_function("gpt-4-0125-preview", context, prompt, temperature, top_p, max_tokens)
+      answer_gpt40_125 = gpt40_125.choices[0].message
+      tokens_gpt40_125 = gpt40_125["usage"]["completion_tokens"]
+
+   
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+      st.header("**gpt-3.5-turbo**")
+      ":boom: 4.096 Tokens | :calendar: Up to Sep 2021"
+
+      st.markdown("Snapshot of GPT-3.5-turbo from June 13th 2023. Best in terms of performance-cost ratio for simple textual tasks")
+      st.link_button("Model", url="https://platform.openai.com/docs/models/gpt-3-5", help=None, type="secondary", disabled=False, use_container_width=False)
+
+      cola, colb, colc = st.columns(3)
+      with cola:
+        st.metric(":boom: Tokens", value = str(tokens_gpt35)+"T", help = "Tokens can be thought of as pieces of words. This number refers to *completion tokens*. You should be aware also of *prompt tokens* when evaluating full costs.")
+      with colb:
+        st.metric(":money_with_wings: Cost", value = str(round(tokens_gpt35*0.002,2))+"$", help ="Cost in $ per 1 thousand operations.")
+      with colc:
+        "#"
+      
+      st.info(answer_gpt35['content'], icon=None)
+      st.checkbox(key = "gpt35love", label="I prefer GPT-3.5-turbo answer", on_change = write_postgres, args=(prompt, context, temperature, top_p, max_tokens, model_1, model_2, model_3, answer_gpt35['content'], answer_gpt40['content'], answer_gpt40_125['content'], tokens_gpt35, tokens_gpt40, tokens_gpt40_125, True, False, False,))
+      
+    
+      
+    with col2:
+      st.header("**gpt-4**")
+      ":boom: 8.192 Tokens | :calendar: Up to Sep 2021"
+    
+      st.markdown("GPT-4 is a large multimodal model optimized for chat and traditional completions tasks with advanced reasoning capabilities")
+      st.link_button("Model", url="https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo", help=None, type="secondary", disabled=False, use_container_width=False)
+      colg, colh, coli = st.columns(3)
+      with colg:
+        st.metric(":boom: Tokens", value = str(tokens_gpt40)+"T", help = "Tokens can be thought of as pieces of words. This number refers to *completion tokens*. You should be aware also of *prompt tokens* when evaluating full costs.")
+      with colh:
+        st.metric(":money_with_wings: Cost", value =str(round(tokens_gpt40*0.06,2))+"$", help ="Cost in $ per 1 thousand operations.")
+      with coli:
+        "#"  
+      st.info(answer_gpt40['content'], icon=None)
+      st.checkbox(key = "gpt40love", label="I prefer GPT-4 answer", on_change = write_postgres, args=(prompt, context, temperature, top_p, max_tokens, model_1, model_2, model_3, answer_gpt35['content'], answer_gpt40['content'], answer_gpt40_125['content'], tokens_gpt35, tokens_gpt40, tokens_gpt40_125, False, True, False,))
 
 
+    with col3:
+      st.header("**gpt-4-0125-preview**")
+      ":boom: 128.000 Tokens | :calendar: Up to Apr 2023"
+    
+      st.markdown("GPT-4 model with reduced cases of laziness and extended context window, where the model doesn't complete the task")
+      st.link_button("Model", url="https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo", help=None, type="secondary", disabled=False, use_container_width=False)
+      cold, cole, colf = st.columns(3)
+      with cold:
+        st.metric(":boom: Tokens", value = str(tokens_gpt40_125)+"T", help = "Tokens can be thought of as pieces of words. This number refers to *completion tokens*. You should be aware also of *prompt tokens* when evaluating full costs.")
+      with cole:
+        st.metric(":money_with_wings: Cost", value =str(round(tokens_gpt40_125*0.03,2))+"$", help ="Cost in $ per 1 thousand operations.")
+      with colf:
+        "#"  
+      st.info(answer_gpt40_125['content'], icon=None)
+      st.checkbox(key = "gpt41love", label="I prefer gpt-4-0125-preview answer", on_change = write_postgres, args=(prompt, context, temperature, top_p, max_tokens, model_1, model_2, model_3, answer_gpt35['content'], answer_gpt40['content'], answer_gpt40_125['content'], tokens_gpt35, tokens_gpt40, tokens_gpt40_125, False, False, True,))
+
+
+
+      with tabInfo:
+   
+        st.subheader("Why this application :question:")
+        st.markdown("Have you ever wondered what the main differences are among OpenAI models? Choosing the right model can be a bit overwhelming. Use this application to compare the results from different models and see the differences for yourself. Yes it's fun, but make sure to check out the documentation afterward! 😊")
+        
+        st.write("**1)** Type your prompt, set up role system and gpt parameters :pencil:")
+        st.write("**2)** Compare results generated by model in term of quality and costs :mag:")
+        st.write("**3)** Choose the answer you prefer. A new answer will be displayed :round_pushpin:")
+        st.info("Please be aware that your queries will be stored anonymously for research and training purposes. Under no circumstances should you insert confidential information.", icon = "⚠️")
+        st.divider()
+        st.subheader("Send me feedbacks :exclamation:")
+        st.markdown("Please fell free to send me feedbacks or ideas to improve the app. You can find me on [linkedin](https://www.linkedin.com/in/giovanni-salvi-5aa278158/) 😊")
+  
+        
+      
+
+
+if __name__ == '__main__':
+    main()
