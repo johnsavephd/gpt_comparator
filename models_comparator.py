80bbae1395125303071148ce170a1e33c709ca14
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-11 22:26:54 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -245,7 +245,7 @@ def main():
         labels = "gpt-3.5-turbo-0125", "gpt-4", "gpt-4-0125-preview"
         sizes = [float(love_1_perc), float(love_2_perc), float(love_3_perc)]
           
-        If love_1_perc + love_2_perc + love_3_perc > 0:
+        if love_1_perc + love_2_perc + love_3_perc > 0:
           fig1, ax1 = plt.subplots()
           ax1.pie(sizes, labels=labels, autopct='%1f%%',
                   shadow=False, startangle=90)
