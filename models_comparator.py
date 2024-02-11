af0d1c3118e61c2ca07a0a6e91315f540b571b7a
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-11 17:22:15 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -249,7 +249,7 @@ def main():
 
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-                shadow=True, startangle=90)
+                shadow=False, startangle=90)
         ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         
         st.pyplot(fig1)
