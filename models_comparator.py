711ff5998dd316c61f9ee29ab57f88df4a4e9418
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-11 15:46:48 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -95,8 +95,8 @@ def write_postgres(user_query, system_role, temperature, top_p, max_tokens, mode
   st.session_state.gpt35love = False
   st.session_state.gpt40love = False
   st.session_state.gpt41love = False
-  st.session_state.prompt = ""
-  st.session_state.context = "You are an helpful assistant"
+  #st.session_state.prompt = ""
+  #st.session_state.context = "You are an helpful assistant"
 
 #--------------------------Sidebar Section-------------------------
 
