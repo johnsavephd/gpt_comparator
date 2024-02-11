1bf50e00f99fc73839d4b58b81307a3a07eb7809
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-11 17:18:27 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -242,8 +242,8 @@ def main():
         st.write(love_3_perc)
         sizes = [float(love_1_perc), float(love_2_perc), float(love_3_perc)]
         st.write(sizes)
-        plt.pie(sizes)
-        plt.show()
+        fig1 = plt.pie(sizes)
+        st.pyplot(fig1)
 
 
     
