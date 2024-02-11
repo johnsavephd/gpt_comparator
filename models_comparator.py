3a1fcb1fecedb5817a3f80c1625ff10ff4c8927e
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-11 22:31:59 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -246,7 +246,7 @@ def main():
         sizes = [float(love_1_perc), float(love_2_perc), float(love_3_perc)]
           
         if love_1_perc + love_2_perc + love_3_perc > 0:
-          fig1, ax1 = plt.subplots(figsize=(3, 3))
+          fig1, ax1 = plt.subplots(figsize=(2, 2))
           ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
                   shadow=False, startangle=90)
           ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
