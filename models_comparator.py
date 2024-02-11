9ea5fdfe3e709132b6dfb2afd7aa43a3c35a8937
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-11 17:11:48 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -241,7 +241,7 @@ def main():
         st.write(love_2_perc)
         st.write(love_3_perc)
         sizes = [love_1_perc, love_2_perc, love_3_perc]
-        plt.pie(sizes, autopct='%1.1f%%', startangle=90)
+        plt.pie(sizes, startangle=90)
         plt.show()
     
 
