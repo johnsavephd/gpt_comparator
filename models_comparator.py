83a6574ab8882714185ef582d905f7db1f466d9e
gsalvi_italy <130920232+johnsavephd@users.noreply.github.com>
2024-02-12 23:30:42 +0100

    Update models_comparator.py

--- models_comparator.py
+++ models_comparator.py
@@ -293,13 +293,14 @@ def main():
           st.markdown("Are you a **developer** looking for **integrating a GPT model** in your application? Have you ever wondered what the main differences are among them? Choosing the right model can be a bit overwhelming. Use this application to compare the results from different models and see the differences for yourself. Yes, but **make sure to check out the documentation** afterward! 😊")
           st.subheader("Pay attention to this :zap:")
           st.markdown("Please be mindful of the fact that **any queries you submit will be stored** in an anonymized manner, exclusively for research and training purposes. It is imperative to note that under no circumstances should you incorporate or divulge any **confidential or sensitive information** in your interactions. Your cooperation in this regard ensures the integrity of our commitment to privacy and data security")
-   
+          st.subheader("Send me feedbacks :exclamation:")
+          st.markdown("Please fell free to send me feedbacks or ideas to improve the app. You can find me on [linkedin](https://www.linkedin.com/in/giovanni-salvi-5aa278158/) 😊")
+          
         with c2:
         #st.info("Please be aware that your queries will be stored anonymously for research and training purposes. Under no circumstances should you insert confidential information.", icon = "⚠️")
           st.subheader("The process :three_button_mouse:")
           st.image("process_image.png")
-          st.subheader("Send me feedbacks :exclamation:")
-          st.markdown("Please fell free to send me feedbacks or ideas to improve the app. You can find me on [linkedin](https://www.linkedin.com/in/giovanni-salvi-5aa278158/) 😊")
+          
 
 if __name__ == '__main__':
     main()
